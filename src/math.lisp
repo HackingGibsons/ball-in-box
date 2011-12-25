b1ab8cbab1225714226ca65e51f50ef8a3a0fe1f
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-12-25 02:32:06 -0500

    [collision] Implementing a few more vector functions

--- src/math.lisp
+++ src/math.lisp
@@ -8,3 +8,11 @@
 (defmethod diff ((v1 vector) (v2 vector))
   "Vector difference of `v1' and `v2'"
   (map 'vector #'- v1 v2))
+
+(defmethod mag ((v1 vector))
+  "Vector magnitude of `v1'"
+  (sqrt (dot v1 v1)))
+
+(defmethod diff-mag ((v1 vector) (v2 vector))
+  "The magnitude of a difference of `v1' `v2'"
+  (mag (diff v1 v2)))
