878770f39d82b15427ac2a9b4b7f6000af36ebb3
Sean Bryant <sean@hackinggibsons.com>
2011-12-26 01:39:59 -0500

    [master] MORE MATH! :cake:

--- src/math.lisp
+++ src/math.lisp
@@ -9,9 +9,25 @@
   "Dot product of two vectors: `v1' . `v2'"
   (reduce #'+ (map 'vector #'* v1 v2)))
 
+(defmethod mag+ ((v1 vector) &optional (scale 1))
+  (let ((normalized (map 'vector #L(* !1 scale) (normalize v1))))
+    (sum v1 normalized)))
+
+(defmethod normalize ((v1 vector))
+  (let ((mag (mag v1)))
+    (map 'vector #L(/ !1 mag) v1)))
+
+(defmethod angle ((v1 vector) (v2 vector))
+  (acos (dot (normalize v1)
+             (normalize v2))))
+
 (defmethod diff ((v1 vector) (v2 vector))
   "Vector difference of `v1' and `v2'"
   (map 'vector #'- v1 v2))
+
+(defmethod sum ((v1 vector) (v2 vector))
+  "Vector sum of `v1' and `v2'"
+  (map 'vector #'+ v1 v2))
 
 (defmethod mag ((v1 vector))
   "Vector magnitude of `v1'"
